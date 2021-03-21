485ce3ba513e1622ea46b6257adc393a965aa239
Alexander Velichko <alvi@mail.ua>
2021-03-21 21:34:16 +0200

    add mail send

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,48 @@
 const express = require('express')
 const router = express.Router()
-const { products, skills } = require('../data.json')
+const nodemailer = require('nodemailer');
+const {products, skills} = require('../data.json')
+const config = require('../config.json');
 
 router.get('/', (req, res, next) => {
-  res.render('pages/index', { title: 'Main page', products, skills })
+  res.render('pages/index', {title: 'Main page', products, skills})
 })
 
 router.post('/', (req, res, next) => {
-  // TODO: Реализовать функционал отправки письма.
-  res.send('Реализовать функционал отправки письма')
+  if (!req.body.name || !req.body.email || !req.body.message) {
+    return res.render('pages/index', {
+      title: 'Main page',
+      products,
+      skills,
+      msgemail: 'Все поля нужно заполнить!',
+    })
+  }
+  const transporter = nodemailer.createTransport(config.mail.smtp)
+  const mailOptions = {
+    from: `"${req.body.name}" <${req.body.email}>`,
+    to: config.mail.smtp.auth.user,
+    subject: config.mail.subject,
+    text:
+      req.body.message.trim().slice(0, 500) +
+      `\n Отправлено с: <${req.body.email}>`
+  }
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      return res.render('pages/index', {
+        title: 'Main page',
+        products,
+        skills,
+        msgemail: `При отправке письма произошла ошибка!: ${error}`
+      })
+    }
+    return res.render('pages/index', {
+      title: 'Main page',
+      products,
+      skills,
+      msgemail: `Сообщение успешно отправлено!`
+    })
+  })
 })
 
 module.exports = router
